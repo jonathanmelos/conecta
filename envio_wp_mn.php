86405bf6b7c34e0aa03ec59cb9ba86db2ecc2d2c
jonathanmelos <jonathanmelo0001@gmail.com>
2025-12-05 11:17:53 -0500

    Remove archivos inecesarios

--- envio_wp_mn.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('conexion.php');
-$id = $_GET['id'];
-$sql = "UPDATE calendario SET estado = 'C' WHERE id_cal = $id";
-$result = mysqli_query($conn,$sql); 
-header('Location: calendario.php');
-?>
